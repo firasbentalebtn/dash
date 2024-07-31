d9e1aa70dcd7a45d95c93d69c5fe0d15cbee4f82
Firas Bentaleb <firasbentalebdev@gmail.com>
2024-07-31 11:42:04 +0100

    db setup

--- server/index.js
+++ server/index.js
@@ -29,3 +29,10 @@ app.use("/client",clientRoutes)
 app.use("/general",generalRoutes)
 app.use("management",managementRoutes)
 app.use("sales",salesRoutes)
+
+
+/*mogoose setup*/
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  app.listen(PORT,() => console.log(`Server listening on ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
