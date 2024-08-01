b9bf0eb21a666ac1d24e52007c785f0408f080b3
Firas Bentaleb <firasbentalebdev@gmail.com>
2024-08-01 11:45:43 +0100

    mui colors and light& dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import {CssBaseline,ThemeProvider} from "@mui/material";
+import {createTheme} from "@mui/material";
+import {themeSettings} from "./theme";
+import {useSelector} from "react-redux";
+import {useMemo} from "react";
 
 function App() {
+
+const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode]);
   return (
     <div className="app">
-
+<ThemeProvider theme={theme}>
+  <CssBaseline />
+      {/* Your App Components */}
+</ThemeProvider>
     </div>
   );
 }
